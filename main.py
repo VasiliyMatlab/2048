f8bd8150df44c7e79baa186bfeaa4fc73fc35ea7
VasiliyMatlab <vasiliy0399@yandex.ru>
2021-08-20 12:16:20 +0300

    Added GUI
    - добавил файл '.gitignore'
    - добавил необходимые константы
    - импортировал модуль 'pygame' и отрисовал экран
    - реализовал событие, по которому при нажитии клавиши добавляется заполненная ячейка и отображается на экране

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+import sys
 import random
+import pygame
+
+
+COLORS = {
+    0: (130, 130, 130),
+    2: (255, 255, 255),
+    4: (255, 255, 128),
+    8: (255, 255, 0)
+}
+WHITE = (255, 255, 255)
+GRAY  = (130, 130, 130)
+BLACK = (0, 0, 0)
 
 SIZE = 4
+SIZE_BLOCK = 110
+MARGIN = 10
+WIDTH = SIZE*SIZE_BLOCK + (SIZE+1)*MARGIN
+HEIGHT = WIDTH + SIZE_BLOCK
+TITLE_RECT = pygame.Rect(0,0, WIDTH, SIZE_BLOCK)
+
 
 # Печать массива в терминал
 def pretty_print(mas: list) -> None:
@@ -43,20 +62,47 @@ def insert_2_or_4(mas: list, x: int, y: int) -> list:
         mas[x][y] = 4
     return mas
 
+# Отрисовка интерфейса
+def draw_interface():
+    pygame.draw.rect(screen, WHITE, TITLE_RECT)
+    font = pygame.font.SysFont("stxingkai", 70)
+    pretty_print(mas)
+    for row in range(SIZE):
+        for col in range(SIZE):
+            value = mas[row][col]
+            text = font.render(str(value), True, BLACK)
+            w = col*SIZE_BLOCK + (col+1)*MARGIN
+            h = row*SIZE_BLOCK + (row+1)*MARGIN + SIZE_BLOCK
+            pygame.draw.rect(screen, COLORS[value], (w,h, SIZE_BLOCK,SIZE_BLOCK))
+            if value != 0:
+                font_w, font_h = text.get_size()
+                text_x = w + (SIZE_BLOCK-font_w)/2
+                text_y = h + (SIZE_BLOCK-font_h)/2
+                screen.blit(text, (text_x, text_y))
 
-def main():
+
+if __name__ == "__main__":
     mas = [[0]*SIZE for n in range(SIZE)]
     mas[1][2] = 2
     mas[3][0] = 4
 
-    while is_zero_in_mas(mas):
-        input()
-        empty = get_empty_list(mas)
-        random.shuffle(empty)
-        random_num = empty.pop()
-        x, y = get_index_from_number(random_num)
-        mas = insert_2_or_4(mas, x, y)
-        pretty_print(mas)
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("2048")
+    draw_interface()
+    pygame.display.update()
 
-if __name__ == '__main__':
-    main()
+    while is_zero_in_mas(mas):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit(0)
+            elif event.type == pygame.KEYDOWN:
+                # input()
+                empty = get_empty_list(mas)
+                random.shuffle(empty)
+                random_num = empty.pop()
+                x, y = get_index_from_number(random_num)
+                mas = insert_2_or_4(mas, x, y)
+                draw_interface()
+                pygame.display.update()
